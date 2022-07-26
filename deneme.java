41b28893066e2e9e41a8370aa64bbb0b6d325438
arif-2006 <72623062+arif-2006@users.noreply.github.com>
2022-07-26 17:30:40 +0300

    tekli sayılaerınb toplamı
    tekli sayıların toplamını  bulan program

---
+++ deneme.java
@@ -0,0 +1,19 @@
+package Sinif;
+
+import java.util.Scanner;
+
+public class deneme {
+    public static void main(String[] args) {
+        int i;
+        int toplam = 0;
+        Scanner deger = new Scanner(System.in);
+
+        do {
+            System.out.print("Bir Sayi Giriniz : ");
+            i = deger.nextInt();
+            if ((i % 2 == 0) && (i % 4 == 0)) ;
+            toplam += i;
+        } while (i % 2 == 0);
+        System.out.print("Toplam : " + toplam);
+    }
+}
