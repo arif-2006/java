11215dc7b94d9ccbd8ed61ec1cd5affa9edef9c4
arif-2006 <72623062+arif-2006@users.noreply.github.com>
2022-07-28 14:13:35 +0300

    4 ve 5 in katlartı

---
+++ deneme2.java
@@ -0,0 +1,20 @@
+package Sinif;
+import java.util.Scanner;
+public class deneme2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, total;
+        total = 0;
+        System.out.println("lutfen bir sayi giriniz");
+        i = sc.nextInt();
+        {
+            if (i % 4 == 0 && i % 5 == 0) ;
+            {
+                i += total;
+                System.out.println("program bitti...");
+            }
+        }
+    }
+    }
+
+
